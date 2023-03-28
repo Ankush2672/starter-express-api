67391bd6060d9b220d0333f6226a6d7b7307c454
Ankush2672 <113376519+Ankush2672@users.noreply.github.com>
2023-03-28 12:03:35 +0530

    get routes

--- api/bus_routes/controllers.js
+++ api/bus_routes/controllers.js
@@ -8,7 +8,6 @@ module.exports = {
         try{
 
             let token_data = await Common_service.jwt_verify_token(req.headers.authorization);
-            console.log(token_data)
                 if(!token_data || token_data.role !== config.roles.transportIncharge)
                 {
                     res.statusCode = 403;
@@ -60,12 +59,67 @@ module.exports = {
     get_bus_route: async (req,res)=>{
 
         try{
+
             let bus_routes = await modals.routes.find({});
-            console.log(bus_routes);
-            res.send("hello");
+            res.send(bus_routes);
         }catch(error)
         {
             console.log("get bus route error", error);
+        }
+        
+    },
+    update_bus_incharge: async (req,res)=>{
+
+        try{
+            
+            let token_data = await Common_service.jwt_verify_token(req.headers.authorization);
+                if(!token_data || token_data.role !== config.roles.transportIncharge)
+                {
+                    res.statusCode = 403;
+                    return res.json({error: "forbidden", message:"not authorized", reason: "not authorized"});
+                }
+
+                let newbusincharge = req.body.new_bus_incharge;
+                let check_duplicate_bus_incharge = await modals.routes.findOne({bus_incharge: newbusincharge});
+                if(check_duplicate_bus_incharge)
+                {
+                    res.statusCode = 400;
+                    return res.json({error: "Incharge already assigned", message:"Incharge already assigned", reason: "Incharge already assigned"});
+                }
+                
+                let routename = req.body.route_name
+                let bus_routes = await modals.routes.findOneAndUpdate({route_name: routename},{bus_incharge:newbusincharge});
+                if(!bus_routes)
+                {
+                    res.statusCode = 400;
+                    return res.json({error: "Bad Request", message:"Update failed", reason: "Update failed"});
+                }
+                res.send("Updated Successfully");
+        }catch(error)
+        {
+            console.log("update bus incharge error", error);
+        }
+        
+    },
+    update_bus_price: async (req,res)=>{
+        try{
+            let token_data = await Common_service.jwt_verify_token(req.headers.authorization);
+                if(!token_data || token_data.role !== config.roles.transportIncharge)
+                {
+                    res.statusCode = 403;
+                    return res.json({error: "forbidden", message:"not authorized", reason: "not authorized"});
+                }
+                
+            let bus_routes = await modals.routes.findOneAndUpdate({route_name: req.body.route_name},{price_ac: req.body.price_ac,price_non_ac: req.body.price_non_ac});
+                if(!bus_routes)
+                {
+                    res.statusCode = 400;
+                    return res.json({error: "Bad Request", message:"Update failed", reason: "Update failed"});
+                }
+                res.send("Updated Successfully");
+        }catch(error)
+        {
+            console.log("update bus incharge error", error);
         }
         
     },
--- api/bus_routes/routes.js
+++ api/bus_routes/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const {add_bus_route,get_bus_route} = require('./controllers');
+const {add_bus_route,get_bus_route,update_bus_incharge,update_bus_price} = require('./controllers');
 
 router.post('/addbusroute',add_bus_route);
-router.get('/',get_bus_route);
+router.get('/getbusroute',get_bus_route);
+router.put('/updatebusincharge',update_bus_incharge);
+router.put('/updatebusprice',update_bus_price);
 
 module.exports = router;
